a849b81d62546aa1ee3ca9458cf59e4858162a0c
hwalborn <hwalborn@gmail.com>
2017-03-22 20:03:27 -0400

    I feel like we could've used that at the begining......

---
+++ index.js
@@ -0,0 +1,27 @@
+var theTruth = () => {
+  return true
+}
+
+var sayHey = () => {
+  return 'Hey!'
+}
+
+var sayHeyFriend = (friend) => {
+  return `Hey, ${friend}!`
+}
+
+var shouting = (phrase) => {
+  return phrase.toUpperCase()
+}
+
+var roundDown = (num) => {
+  return Math.floor(num)
+}
+
+var favoriteIceCream = (flavor) => {
+  if(flavor === 'mint chocolate chip'){
+    return 'I love mint chocolate chip'
+  }else{
+    return "Meh"
+  }
+}
